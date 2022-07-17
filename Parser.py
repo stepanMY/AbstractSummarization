fd2d2200d690d8f443ea62f66d7d2bdc89772946
stepanMY <o.malih2012@gmail.com>
2022-07-17 18:28:42 +0300

    added parser

---
+++ Parser.py
@@ -0,0 +1,67 @@
+import asyncio
+import aiohttp
+import nest_asyncio
+nest_asyncio.apply()
+
+
+class AsyncParser:
+    """ Class that parses webpages asynchronously """
+
+    def __init__(self,
+                 urls,
+                 process,
+                 n_connections,
+                 n_retries=5,
+                 retrywait=0.5):
+        """
+        @param urls list of web addresses to parse
+        @param process function that accepts url and page text, handles it
+               and returns result
+        @param n_connections limit of simultaneously opened connections
+        @param n_retries number of possible retries
+        @param retrywait seconds to wait before retrying to reach url
+        """
+        self.urls = urls
+        self.process = process
+        self.n_connections = n_connections
+        self.n_retries = n_retries
+        self.retrywait = retrywait
+        self.ranflag = False
+        self.notreachedurls = []
+
+    async def geturl(self, session, url):
+        """ Processes one url """
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                text_ = await resp.text()
+                return self.process(url, text_)
+        async with session.get(url) as resp:
+            for _ in range(self.n_retries):
+                if resp.status == 200:
+                    text_ = await resp.text()
+                    return self.process(url, text_)
+            self.notreachedurls.append(url)
+            return None
+
+    async def geturls(self):
+        """ Processes all urls """
+
+        connector = aiohttp.TCPConnector(limit=self.n_connections)
+        async with aiohttp.ClientSession(connector=connector) as session:
+            tasks = []
+            for url in self.urls:
+                tasks.append(asyncio.ensure_future(self.geturl(session, url)))
+
+            results = await asyncio.gather(*tasks)
+            return results
+
+    def parse(self):
+        """ Start parsing job """
+        self.ranflag = True
+        return asyncio.run(self.geturls())
+
+    def notreached(self):
+        """ Returns list of not reached urls """
+        if self.ranflag:
+            raise AttributeError('Didn\'t parse')
+        return self.notreachedurls
