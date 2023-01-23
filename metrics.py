716eff7fdaed081acab5c92635edbb65df03f07d
stepanMY <o.malih2012@gmail.com>
2023-01-24 00:49:28 +0300

    added meteor

--- metrics.py
+++ metrics.py
@@ -2,6 +2,7 @@ from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
 from nltk.translate.chrf_score import corpus_chrf
 from rouge import Rouge
 from collections import Counter
+from meteor import Meteor
 
 
 def bleu_score(y_true, y_pred, smoothing=True):
@@ -47,6 +48,22 @@ def rouge_score(y_true, y_pred):
     return scores_dict
 
 
+def meteor_score(y_true, y_pred, meteor_jar, language='ru'):
+    """
+    Calculates corpus meteor scores
+
+    @param y_true: list of lists, true tokenized values
+    @param y_pred: list of lists, hypothetical tokenized values
+    @param meteor_jar: .jar file name, synonyms and stemming for calculation
+    @param language: string, language name; 'ru' for russian
+    """
+    y_true_ = [[' '.join(i)] for i in y_true]
+    y_pred_ = [' '.join(i) for i in y_pred]
+    meteor = Meteor(meteor_jar, language=language)
+    scores = meteor.compute_score(y_pred_, y_true_)
+    return scores
+
+
 def calc_duplicate_n_grams_rate(y_pred):
     """
     Calculates weighted n-grams duplicate rate in references
@@ -66,12 +83,13 @@ def calc_duplicate_n_grams_rate(y_pred):
     return scores_dict
 
 
-def calc_metrics(y_true, y_pred):
+def calc_metrics(y_true, y_pred, meteor_jar):
     """
     Calculates corpus metrics
 
     @param y_true: list of lists, true tokenized values
     @param y_pred: list of lists, hypothetical tokenized values
+    @param meteor_jar: .jar file name, synonyms and stemming for calculation
     """
     metrics = dict()
     metrics['corpus_size'] = len(y_true)
@@ -83,6 +101,8 @@ def calc_metrics(y_true, y_pred):
     metrics['rouge-1-f'] = rouge_scores['rouge-1']['f']
     metrics['rouge-2-f'] = rouge_scores['rouge-2']['f']
     metrics['rouge-l-f'] = rouge_scores['rouge-l']['f']
+    met_score = meteor_score(y_true, y_pred, meteor_jar)
+    metrics['meteor'] = met_score
     dup_scores = calc_duplicate_n_grams_rate(y_pred)
     metrics['duplicate_ngrams'] = dup_scores
     lengths = [len(i) for i in y_pred]
@@ -90,16 +110,17 @@ def calc_metrics(y_true, y_pred):
     return metrics
 
 
-def print_corp_metrics(y_true, y_pred):
+def print_corp_metrics(y_true, y_pred, meteor_jar):
     """
     Prints corpus metrics in percents
 
     @param y_true: list of lists, true tokenized values
     @param y_pred: list of lists, hypothetical tokenized values
+    @param meteor_jar: .jar file name, synonyms and stemming for calculation
     """
     delim = '-------------METRICS-------------'
     print(delim)
-    metrics = calc_metrics(y_true, y_pred)
+    metrics = calc_metrics(y_true, y_pred, meteor_jar)
     print('Corpus Size:   ', metrics['corpus_size'])
     print('Ref Example:   ', metrics['ref_example'])
     print('Hyp Example:   ', metrics['hyp_example'])
@@ -108,6 +129,7 @@ def print_corp_metrics(y_true, y_pred):
     print('ROUGE-1-F:\t{:3.1f}'.format(metrics['rouge-1-f'] * 100.0))
     print('ROUGE-2-F:\t{:3.1f}'.format(metrics['rouge-2-f'] * 100.0))
     print('ROUGE-L-F:\t{:3.1f}'.format(metrics['rouge-l-f'] * 100.0))
+    print('METEOR:   \t{:3.1f}'.format(metrics['meteor'] * 100.0))
     print('Dup 1-grams:\t{:3.1f}'.format(metrics['duplicate_ngrams'][1] * 100.0))
     print('Dup 2-grams:\t{:3.1f}'.format(metrics['duplicate_ngrams'][2] * 100.0))
     print('Dup 3-grams:\t{:3.1f}'.format(metrics['duplicate_ngrams'][3] * 100.0))
