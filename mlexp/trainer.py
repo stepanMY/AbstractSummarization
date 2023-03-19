c2e804d7fcda94e688192f50c99623a4974be4f7
stepanmy <o.malih2012@gmail.com>
2023-03-20 02:57:15 +0300

    trainer-beta

--- mlexp/trainer.py
+++ mlexp/trainer.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 import random
+from tqdm.auto import tqdm
+from util.tokenizator import tokenize_sentences
 
 
 class Trainer:
@@ -30,6 +32,7 @@ class Trainer:
         Should have log_metric, log_stepmetric methods
         @param checkpoint_path: string, directory of checkpoints
         @param metrics_func: function, should accept y_true and y_pred and calculate metrics
+        Should accept y_true and y_pred as list of lists (tokenized)
         @param metrics: list of strings, metrics to use
         Should be calculated by metrics_func
         @param diagnostics: list of strings, diagnostic metrics to use
@@ -48,7 +51,7 @@ class Trainer:
         """
         self.model_shell = model_shell
         self.logger = logger
-        self.checkpoints_path = checkpoint_path
+        self.checkpoint_path = checkpoint_path
         self.metrics_func = metrics_func
         self.metrics = metrics
         self.diagnostics = diagnostics
@@ -70,11 +73,105 @@ class Trainer:
         self.val_epoch = val_epoch
         self.saveafter_epoch = saveafter_epoch
 
-        def train(self, train_dataloader, val_dataloader):
-            """
-            Train model
+        self.train_loss = []
+        self.step_curr = 0
+        self.train_curr, self.val_curr = 0, 0
+        self.best_valmetrics = {metric: 0 for metric in self.metrics}
+        self.was_saved = False
 
-            @param train_dataloader: pytorch dataloader, training samples
-            @param val_dataloader: pytorch dataloader, validation samples
-            """
-            return
+    def log_trainloss(self, step):
+        """
+        Log intermediate loss value and lr
+
+        @param step: float, current step in epochs
+        """
+        self.logger.log_stepmetric('training_procedure/trainloss', np.mean(self.train_loss), step)
+        self.logger.log_stepmetric('training_procedure/lr', self.scheduler.get_last_lr()[0], step)
+        return
+
+    def log_valmetrics(self, val_metrics, step):
+        """
+        Log intermediate validation metrics
+
+        @param val_metrics: dict, current validation metrics
+        @param step: float, current step in epochs
+        """
+        for metric in self.metrics:
+            self.logger.log_stepmetric(f'val/{metric}', val_metrics[metric], step)
+        for diagnostic in self.diagnostics:
+            self.logger.log_stepmetric(f'val/{diagnostic}', val_metrics[diagnostic], step)
+        return
+
+    def validate(self, val_dataloader):
+        """
+        Validate model, calculate metrics on val_dataloader
+
+        @param val_dataloader
+        """
+        self.model_shell.model.eval()
+        ytrue_val, ypred_val = [], []
+        for batch_val in val_dataloader:
+            x_val, y_val = list(batch_val[0]), list(batch_val[1])
+            ytrue_val.extend(y_val)
+            preds_batch = self.model_shell.generate(x_val)
+            ypred_val.extend(preds_batch)
+        val_metrics = self.metrics_func(tokenize_sentences(ytrue_val),
+                                        tokenize_sentences(ypred_val))
+        return val_metrics
+
+    def save_condition(self, val_metrics):
+        """
+        Check whether model should be saved
+
+        @param val_metrics: dict, current validation metrics
+        """
+        valcounter = 0
+        for metric in self.metrics:
+            if val_metrics[metric] >= self.best_valmetrics[metric]:
+                valcounter += 1
+        if valcounter >= len(self.metrics)/2:
+            return True
+        return False
+
+    def update_best(self, val_metrics):
+        """
+        Update best validation metrics
+
+        @param val_metrics: dict, current validation metrics
+        """
+        for metric in self.metrics:
+            self.best_valmetrics[metric] = val_metrics[metric]
+
+    def train(self, train_dataloader, val_dataloader):
+        """
+        Train model while reporting intermediate train loss and validation metrics
+
+        @param train_dataloader: pytorch dataloader, training samples
+        @param val_dataloader: pytorch dataloader, validation samples
+        """
+        self.model_shell.model.train()
+        for epoch in range(self.epochs):
+            for i, batch in enumerate(tqdm(train_dataloader)):
+                epoch_curr = self.step_curr/len(train_dataloader)
+                x, y = list(batch[0]), list(batch[1])
+                loss = self.model_shell.calc_loss(x, y)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                self.scheduler.step()
+                self.train_loss.append(loss.item())
+                if epoch_curr >= self.train_curr:
+                    self.log_trainloss(epoch_curr)
+                    self.train_loss = []
+                    self.train_curr += self.train_curr
+                if epoch_curr >= self.val_curr:
+                    val_metrics = self.validate(val_dataloader)
+                    self.log_valmetrics(val_metrics, epoch_curr)
+                    if epoch_curr >= self.saveafter_epoch:
+                        if self.save_condition(val_metrics):
+                            self.update_best(val_metrics)
+                            self.model_shell.save_model(self.checkpoint_path)
+                            self.was_saved = True
+                    self.model_shell.model.train()
+                    self.val_curr += self.val_epoch
+                self.step_curr += 1
